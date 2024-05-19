13554cd6e0aca891b042fcdb5cf475da26216028
João Ferreira <me.joaoferreira@gmail.com>
2024-05-19 22:19:31 +0100

    feat(server): setup project

---
+++ server/src/main/kotlin/flip/todo/Application.kt
@@ -0,0 +1,7 @@
+package flip.todo
+
+import io.micronaut.runtime.Micronaut.run
+fun main(args: Array<String>) {
+	run(*args)
+}
+
---
+++ server/src/test/kotlin/flip/todo/ServerTest.kt
@@ -0,0 +1,19 @@
+package flip.todo
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import jakarta.inject.Inject
+
+@MicronautTest
+class ServerTest {
+
+    @Inject
+    lateinit var application: EmbeddedApplication<*>
+
+    @Test
+    fun testItWorks() {
+        Assertions.assertTrue(application.isRunning)
+    }
+
+}
